75e83649921ab0d14ec0f704c5149c03f7c270b8
FrancisML <francismlasalvia@gmail.com>
2025-01-08 00:46:15 -0500

    Initial Commit

---
+++ Calorie counter/Calorie_counterApp.swift
@@ -0,0 +1,32 @@
+//
+//  Calorie_counterApp.swift
+//  Calorie counter
+//
+//  Created by frank lasalvia on 1/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Calorie_counterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
